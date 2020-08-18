3ceab84cd6e13849ddd0d24ead3761f1d80c0873
Ron Gerlach <wordsmith4u@hotmail.com>
2020-08-17 19:01:47 -0500

    added bmdb folder

---
+++ bmdb/Capstone_BMDb_Create_v01_only_movie.sql
@@ -0,0 +1,22 @@
+-- create and select the database
+DROP DATABASE IF EXISTS bmdb;
+CREATE DATABASE bmdb;
+USE bmdb;
+
+-- create Movie table
+-- DROP TABLE IF EXISTS Movie;
+Create table Movie (
+ID 			integer 		primary key auto_increment,
+Title 		varchar(255) 	not null unique,
+Year 		integer 		not null,
+Rating 		varchar(5) 		not null,
+Director 	varchar(255) 	not null
+-- CONSTRAINT utitle unique (Title)
+);
+
+-- Add some movies
+ insert into Movie VALUES
+ 	(1, 'Star Wars', 1977, 'PG', 'George Lucas'),
+ 	(2, 'Sixteen Candles', 1984, 'PG', 'John Hughes'),
+  	(3, 'Fifth Element', 1997, 'PG-13', 'Luc Besson'),
+ 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland');
